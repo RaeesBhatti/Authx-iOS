23284517c0202f46a9b8dac1e61176467e91f28e
Raees Iqbal <raees.bhatti@gmail.com>
2020-06-06 19:32:32 -0700

    First milestone on the UI

--- authx/ContentView.swift
+++ authx/ContentView.swift
@@ -8,17 +8,66 @@
 
 import SwiftUI
 
+struct Credential {
+    var Id: UUID
+    var Issuer: String
+    var AccountName: String
+    var Key: String
+}
+
+struct CredentialRow: View {
+    var credential: Credential
+
+    var body: some View {
+        HStack {
+            Image(systemName: "icloud.fill")
+                .resizable()
+                .frame(maxWidth: 40, maxHeight: 40)
+                .padding()
+                .aspectRatio(contentMode: .fit)
+            VStack (alignment: .leading, spacing: 8) {
+                Text(credential.Issuer)
+                    .font(.headline)
+                Text(credential.AccountName)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct CredentialsList: View {
+    var credentialsData: Array<Credential>
+    var body: some View {
+        List(credentialsData, id: \.Id) { credential in
+            CredentialRow(credential: credential)
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var selection = 0
+    @State var credentialsData = [
+        Credential(Id: UUID.init(), Issuer: "Google", AccountName: "Something", Key: ""),
+        Credential(Id: UUID.init(), Issuer: "Twitter", AccountName: "Something", Key: ""),
+        Credential(Id: UUID.init(), Issuer: "Facebook", AccountName: "Something", Key: ""),
+        Credential(Id: UUID.init(), Issuer: "Github", AccountName: "Something", Key: ""),
+    ]
+    @State var activeCredential: Credential = nil
  
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
+            NavigationView {
+                VStack{
+                    Text("Password here!")
+                    CredentialsList(credentialsData: credentialsData)
+                }
+                .navigationBarItems(trailing: Image(systemName: "plus"))
+            }
                 .tabItem {
                     VStack {
-                        Image("first")
-                        Text("First")
+                        Image(systemName: "lock.shield")
+                        Text("Accounts")
                     }
                 }
                 .tag(0)
@@ -26,8 +75,8 @@ struct ContentView: View {
                 .font(.title)
                 .tabItem {
                     VStack {
-                        Image("second")
-                        Text("Second")
+                        Image(systemName: "gear")
+                        Text("Settings")
                     }
                 }
                 .tag(1)
